727ddc57eb81fbc990bbda9b263e87627b2b5882
Bikramjeet Sarmah <9854bikram@gmail.com>
2023-01-02 14:31:15 +0530

    Frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,9 @@
+// import component from "@components"
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-gray-800">
+      <h1 className="text-white text-5xl">Hello its working</h1>
+    </div>
+  );
+}
